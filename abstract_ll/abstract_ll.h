469a606cd2bf13ab0c386c1deba77f24f0897435
me <me@maincomp>
2018-02-04 13:57:54 -0800

    Worked on abstract linked list
    Changed function to accept void**
    Fix existing functions and added new ones
    Did some testing on input parameters to
       prevent seg faults

--- abstract_ll/abstract_ll.h
+++ abstract_ll/abstract_ll.h
@@ -3,13 +3,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-//Creates a linked list
-void *create_ll( int count, int element_size);
+//Creates a linked list. Callers would do something like
+//test_t *my_ll_head;
+//create_ll( (void**)&my_ll_head, <number>, sizeof(test_t));
+void create_ll( void **list, int count, int element_size);
 
-//Deallocates each node of a linked list
-void dealloc_ll(void *list);
+//Deallocates each node of a linked list.
+//Callers would do
+//dealloc_ll( (void**)&my_ll_head );
+void dealloc_ll(void **list);
 
 //Adds an element to the list at the given index
 //Does nothing if the index is invalid (i.e. adding
 //an element to index 5 in a 3 element list)
-void *add_node_ll( void *list, void *element, int index);
+int add_node_ll( void **list, void *element, int index);
+
+//Removes the given element from the list and returns it.
+void *rm_node_ll( void **list, int index );
+
+//Returns the given node at index or null if it does
+//not exist
+void *get_node_ll( void **list, int index );
+
+//Replaces the node at index and returns the node that
+//was there
+void *set_node_ll(void **list, void *new_node, int index);
--- test.c
+++ test.c
@@ -12,34 +12,75 @@ typedef struct mem{
    struct mem *next;
 }mem_t;
 
-/*
-void *rm_node_ll( void *list, int index){
-   if( index == 0 ){
-      
-   }
-}*/
+//Find the given element in the list, using a compare function
+//passed in.  Return null if it is not found.
+void *find_node_ll( void **list, void *element, int (*compare_function)(void *first, void *second) ){
+
+    mem_t **head = (mem_t**)list;
+    mem_t *stepper = *head;
+
+    while( stepper != NULL ){
+       if( compare_function( stepper, element ) == 1 )
+          return stepper;
+       stepper = stepper->next;
+    }
+    return 0;
+}
+
+int comp( void *, void *element2){
+   printf("%d\n", ((test_t*)element1)->e);
+   if( ((test_t*)element1)->e > ((test_t*)element2)->e )
+      return 1;
+   else
+      return 0;
+}
 
 int main(){
 
-   test_t **a;
-   test((void**)a);
+   test_t *element = (test_t*)malloc(sizeof(test_t));
+   element->e = 11;
+   test_t *el2 = (test_t*)malloc(sizeof(test_t));
+   el2->e = 22;
+
+   test_t *compare = (test_t*)malloc(sizeof(test_t));
+   compare->e = 10;
+
+   test_t *t;
+
+   create_ll( (void**)&t, 0, sizeof(test_t));
+   add_node_ll( (void**)&t, element, 0 );
+   add_node_ll( (void**)&t, el2, 1);
+
+//   test_t *removed = (test_t*)rm_node_ll( (void**)&t, 0 );
+//   printf("%d %d\n", removed->e, removed->next);
+   test_t *found_node = find_node_ll( (void**)&t, compare, comp );
+   if( found_node )
+      printf("%d %d\n", found_node->e, found_node->next );
+
+
+   dealloc_ll( (void**)&t );
+
 
 /*
-   test_t *a = (test_t*)create_ll( 3, sizeof(test_t));
+   test_t *b = (test_t*)malloc(sizeof(test_t));
+   b->e = 100;
+   test_t *c = (test_t*)malloc(sizeof(test_t));
+   c->e = 88;
+   test_t *d = (test_t*)malloc(sizeof(test_t));
+   d->e = 77;
 
-   a->e = 10;
-   a->next->e = 20;
-   a->next->next->e = 30;
+   test_t *a;
+   create_ll( (void**)&a, 0, sizeof(test_t));
+   add_node_ll( (void**)&a, b, 0 );
+   add_node_ll( (void**)&a, c, 1);
+
+   test_t *temp = (test_t*)set_node_ll( (void**)&a, d, 1 );
+
+   printf("%d %d\n", temp->e, temp->next );
+   printf("%d %d\n", a->next->e, a->next->next);
    
-   printf("%d %d %d\n", a->e, a->next->e, a->next->next->e);
 
-   test_t *new_el = (test_t*)malloc(sizeof(test_t));
-   new_el->e = 111;
-
-   a = add_node_ll( a, new_el, 3 );
-   printf("%d %d %d %d\n", a->e, a->next->e, a->next->next->e, a->next->next->next->e);
-
-   dealloc_ll( a );
+   dealloc_ll( (void**)&a );
 */
    return 0;
 }
